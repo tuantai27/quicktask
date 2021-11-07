68f9b7b35b4c605ade9da897e01139597c1d7b96
tuantai27 <30619152+tuantai27@users.noreply.github.com>
2021-11-07 14:42:46 +0700

    Update pageThanks.js
    a

--- modules/pageThanks.js
+++ modules/pageThanks.js
@@ -23,20 +23,24 @@ async function updateStatus(req, res, next) {
 
 async function trigger(req, res, next) {
     try {
-        var spawn = require('child_process').spawn,
-        ls    = spawn('cmd.exe', ['/c', 'test.bat']);
+        await new Promise((resolve, reject) => {
+            var spawn = require('child_process').spawn,
+            ls    = spawn('cmd.exe', ['/c', '/Users/Administrator/test.bat']);
 
-        ls.stdout.on('data', function (data) {
-            console.log('stdout: ' + data);
-        });
+            ls.stdout.on('data', function (data) {
+                console.log('stdout: ' + data);
+            });
 
-        ls.stderr.on('data', function (data) {
-            console.log('stderr: ' + data);
-        });
+            ls.stderr.on('data', function (data) {
+                console.log('stderr: ' + data);
+                reject(data);
+            });
 
-        ls.on('exit', function (code) {
-            console.log('child process exited with code ' + code);
-        });
+            ls.on('exit', function (code) {
+                console.log('child process exited with code ' + code);
+                resolve();
+            });
+        })
         return res.status(200).send({ status :"finish", result: []});
     } catch (error) {
         console.log(error);
