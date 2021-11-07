51ac662ebd23ceeffba4fb53d6e46c28f797cae1
tuantai27 <30619152+tuantai27@users.noreply.github.com>
2021-11-07 13:36:28 +0700

    a
    trigger code

--- modules/pageThanks.js
+++ modules/pageThanks.js
@@ -4,7 +4,9 @@ const router = require('express').Router;
 const output = {};
 
 output.router = router();
+output.router2 = router();
 output.router.get('/:monthly/:uuid' , updateStatus);
+output.router2.get('/' , trigger);
 
 async function updateStatus(req, res, next) {
     try {
@@ -13,6 +15,28 @@ async function updateStatus(req, res, next) {
             status_name : 'Đã Xác nhận.'
         });
         createFileApproval(req.params.uuid);
+    } catch (error) {
+        console.log(error);
+    }
+    res.render('thanks',{});
+};
+
+async function trigger(req, res, next) {
+    try {
+        var spawn = require('child_process').spawn,
+        ls    = spawn('cmd.exe', ['/c', 'test.bat']);
+
+        ls.stdout.on('data', function (data) {
+            console.log('stdout: ' + data);
+        });
+
+        ls.stderr.on('data', function (data) {
+            console.log('stderr: ' + data);
+        });
+
+        ls.on('exit', function (code) {
+            console.log('child process exited with code ' + code);
+        });
     } catch (error) {
         console.log(error);
     }
