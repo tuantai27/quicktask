7ac48a10e03ef9da83bdb48415c380bf979e5a51
tuantai27 <30619152+tuantai27@users.noreply.github.com>
2022-03-05 11:24:59 +0700

    a
    a

--- client/js/main.js
+++ client/js/main.js
@@ -535,9 +535,9 @@ Vue.component('my-date-input', {
                 return '';
             }
             let temp = null;
-            if (typeof date === 'string' && this.stringToDate(date, 'dd-mm-yyyy', '-')) {
-                const dateParts = date.split("-");
-                temp = new Date(+dateParts[0], dateParts[1] - 1, +dateParts[2]); 
+            if (typeof date === 'string') {
+                // const dateParts = date.split("-");
+                return date; 
             } else {
                 temp = date;
             }
