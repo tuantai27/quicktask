9e05c8534a543ade5da0823bc6ec4348e36a521e
tuantai27 <30619152+tuantai27@users.noreply.github.com>
2022-03-05 13:10:17 +0700

    a
    a

--- datalayer/index_data.js
+++ datalayer/index_data.js
@@ -77,7 +77,6 @@ out.iData = async ({id, uuid, meta_data}, {type, input, output}, project_id, tab
         column_name : output,
         table_id    : table_id
     });
-    console.log('a');
     if (column_id) {
         sql.push(`insert into ${table_name} (id, uuid, column_id, uuid_row, ${column_value}) values (?, ?, ?, ?, ?)`);
 
@@ -87,7 +86,6 @@ out.iData = async ({id, uuid, meta_data}, {type, input, output}, project_id, tab
         params.push(uuid_row);
         if (table_name === 'idx_date_data') {
             params.push(value);
-            // datePickerDate.toISOString();
 
         } else {
             params.push(value);
